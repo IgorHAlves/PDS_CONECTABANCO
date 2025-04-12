673fcf50fd378b7523a65402fa260021f2124617
Igor Alves <igor.h.alves.ads@gmail.com>
2025-04-11 22:13:42 -0300

    Projeto faculdade conectar no banco

---
+++ PROJETO/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROJETO
+{
+    public class Pessoa
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Salario { get; set; }
+
+        public Pessoa(string nome, decimal salario)
+        {
+            Nome = nome;
+            Salario = salario;
+        }
+        
+        private Pessoa () { }
+    }
+}
